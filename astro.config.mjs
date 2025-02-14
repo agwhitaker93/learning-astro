9b02ecf69efc4dacbc0ef0a8746536d6537aef64
Andrew Whitaker <agwhitaker-dev@protonmail.com>
2025-02-14 17:40:25 +0000

    remake from cloudflare create command

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
-
-import webVitals from '@astrojs/web-vitals';
-
-import db from '@astrojs/db';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import db from "@astrojs/db";
+import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-  //output: 'server',
-  integrations: [react(), webVitals(), db()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: cloudflare(),
-
-vite: {
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias: import.meta.env.PROD && {
-        "react-dom/server": "react-dom/server.edge",
-      },
-    },
-  },
+  integrations: [react(), db(), webVitals()],
 });
+
