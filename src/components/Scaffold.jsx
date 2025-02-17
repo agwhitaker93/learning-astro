287a1c789cb978a3118e7fc3336a5d8ebc37a747
Andrew Whitaker <andrew.whitaker@icetravelgroup.com>
2025-02-17 10:13:35 +0000

    formatting

--- src/components/Scaffold.jsx
+++ src/components/Scaffold.jsx
@@ -1,9 +1,8 @@
-
-export default function Scaffold({children}) {
-	return (
-	<div>
-	<h1>Welcome to my test component</h1>
-			{children}
-	</div>
-	)
+export default function Scaffold({ children }) {
+  return (
+    <div>
+      <h1>Welcome to my test component</h1>
+      {children}
+    </div>
+  );
 }
